28fb4e116cc0e099c91d0c0df2e3a03324caa11b
nblog <503407184@qq.com>
2025-08-18 10:56:56 +0800

    style(entity): 为review结构增加注释

--- biz/entity/review_entity.py
+++ biz/entity/review_entity.py
@@ -2,20 +2,20 @@ class MergeRequestReviewEntity:
     def __init__(self, project_name: str, author: str, source_branch: str, target_branch: str, updated_at: int,
                  commits: list, score: float, url: str, review_result: str, url_slug: str, webhook_data: dict,
                  additions: int, deletions: int, last_commit_id: str):
-        self.project_name = project_name
-        self.author = author
-        self.source_branch = source_branch
-        self.target_branch = target_branch
-        self.updated_at = updated_at
-        self.commits = commits
-        self.score = score
-        self.url = url
-        self.review_result = review_result
-        self.url_slug = url_slug
-        self.webhook_data = webhook_data
-        self.additions = additions
-        self.deletions = deletions
-        self.last_commit_id = last_commit_id
+        self.project_name = project_name      # 项目名称
+        self.author = author                  # 提交作者
+        self.source_branch = source_branch    # 源分支
+        self.target_branch = target_branch    # 目标分支
+        self.updated_at = updated_at          # 更新时间（时间戳）
+        self.commits = commits                # 提交列表
+        self.score = score                    # 评审分数
+        self.url = url                        # 评审链接
+        self.review_result = review_result    # 评审结果
+        self.url_slug = url_slug              # URL标识
+        self.webhook_data = webhook_data      # webhook数据
+        self.additions = additions            # 新增行数
+        self.deletions = deletions            # 删除行数
+        self.last_commit_id = last_commit_id  # 最后一次提交的ID
 
     @property
     def commit_messages(self):
@@ -26,17 +26,17 @@ class MergeRequestReviewEntity:
 class PushReviewEntity:
     def __init__(self, project_name: str, author: str, branch: str, updated_at: int, commits: list, score: float,
                  review_result: str, url_slug: str, webhook_data: dict, additions: int, deletions: int):
-        self.project_name = project_name
-        self.author = author
-        self.branch = branch
-        self.updated_at = updated_at
-        self.commits = commits
-        self.score = score
-        self.review_result = review_result
-        self.url_slug = url_slug
-        self.webhook_data = webhook_data
-        self.additions = additions
-        self.deletions = deletions
+        self.project_name = project_name      # 项目名称
+        self.author = author                  # 提交作者
+        self.branch = branch                  # 提交分支
+        self.updated_at = updated_at          # 更新时间（时间戳）
+        self.commits = commits                # 提交列表
+        self.score = score                    # 评审分数
+        self.review_result = review_result    # 评审结果
+        self.url_slug = url_slug              # URL标识
+        self.webhook_data = webhook_data      # webhook数据
+        self.additions = additions            # 新增行数
+        self.deletions = deletions            # 删除行数
 
     @property
     def commit_messages(self):
